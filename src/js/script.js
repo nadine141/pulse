9b25b5753d5f68d50c976638b94d78d88114195c
Nadezhda <nadin.fit.01@gmail.com>
2023-05-25 19:11:48 +0300

    tabs+reviews

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 //   $('.carousel__inner').slick({
 //     speed: 1200,
 //     adaptiveHeight: true,
@@ -13,8 +13,8 @@
 //         }
 //       }
 //     ]
-//   });
-// });
+
+
 
 const slider = tns({
   container: '.carousel__inner',
@@ -50,3 +50,23 @@ document.querySelector('.next').addEventListener('click', function () {
 //     }
 //   }
 // });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleClass(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    })
+  };
+  toggleClass('.catalog-item__link');
+  toggleClass('.catalog-item__back');
+
+});
